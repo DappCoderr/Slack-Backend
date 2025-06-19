0f777f1ed4145f7cf8964b1a38ca60652eb2c210
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2025-06-19 14:51:26 +0530

    add routing layer

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import { StatusCodes } from 'http-status-codes';
 
 import { connectDB } from './config/db_config.js';
 import { PORT } from './config/server_config.js';
+import apiRouter from './router/apiRouter.js';
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', apiRouter);
 
 app.get('/', (req, res) => {
   res.status(StatusCodes.OK).json({
