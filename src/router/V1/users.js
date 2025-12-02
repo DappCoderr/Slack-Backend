a2a1288579a213df565d257b89a75b8ab72d032a
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2025-12-02 11:49:44 +0530

    update router layer

--- src/router/V1/users.js
+++ src/router/V1/users.js
@@ -1,8 +1,8 @@
 import express from 'express';
 
 import { signUp } from '../../controller/userController';
-import { validate } from '../../validation/zodValidator';
 import { userSignupSchema } from '../../validation/userSchema';
+import { validate } from '../../validation/zodValidator';
 
 const router = express.Router();
 
