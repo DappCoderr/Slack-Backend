c79947e1486b44a55158a5fb30fcc3f099214226
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2025-12-04 23:43:54 +0530

    update userRepo layer

--- src/repository/userRepository.js
+++ src/repository/userRepository.js
@@ -1,32 +1,9 @@
 import User from '../schema/user.js';
 import { crudRepository } from './crudRepository.js';
 
-const userRepo = crudRepository(User);
+const userRepository = {
+  ...crudRepository(User)
+}
 
-export const getUserByEmail = async (email) => {
-  return userRepo.getByAnyField({ email });
-};
+export default userRepository
 
-export const getUserByName = async (name) => {
-  return userRepo.getByAnyField({ name });
-};
-
-export const getUserById = async (id) => {
-  return userRepo.getById(id);
-};
-
-export const createUser = async (user) => {
-  return userRepo.create(user);
-};
-
-export const getUsers = async () => {
-  return userRepo.getAll();
-};
-
-export const deleteUser = async (id) => {
-  return userRepo.deleteById(id);
-};
-
-export const updateUser = async (id, user) => {
-  return userRepo.updateById(id, user);
-};
