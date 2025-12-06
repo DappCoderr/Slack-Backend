146d7f0678b344213827b3e11f3a21b5145f8481
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2025-12-06 22:15:35 +0530

    update controller

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,32 +1,45 @@
 import { StatusCodes } from 'http-status-codes';
-import { signInService, signUpService } from '../service/userService';
-import {customErrorResponse, internalServerError, successResponse} from '../utils/common/responseObject';
+
+import { signInService, signUpService } from '../service/userService.js';
+import {
+  customErrorResponse,
+  internalErrorResponse,
+  successResponse
+} from '../utils/common/responseObject.js';
 
 export const signUp = async (req, res) => {
   try {
     const user = await signUpService(req.body);
+
     return res
       .status(StatusCodes.CREATED)
-      .json(successResponse(user, 'User Created Successfully'));
+      .json(successResponse(user, 'User created successfully'));
   } catch (error) {
-    console.lor('User controller error: ', error);
+    console.log('User controller error', error);
     if (error.statusCode) {
       return res.status(error.statusCode).json(customErrorResponse(error));
     }
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(internalServerError(error));
+
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json(internalErrorResponse(error));
   }
 };
 
-export const signIn = async(req, res) => {
+export const signIn = async (req, res) => {
   try {
-    const response = await signInService(req.body)
-    return res.status(StatusCodes.CREATED).json(successResponse(response, "User signed successfully"))
-    
+    const response = await signInService(req.body);
+    return res
+      .status(StatusCodes.OK)
+      .json(successResponse(response, 'User signed in successfully'));
   } catch (error) {
-    console.log("SignIn error: ", error)
+    console.log('User controller error', error);
     if (error.statusCode) {
       return res.status(error.statusCode).json(customErrorResponse(error));
     }
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(internalServerError(error));
+
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json(internalErrorResponse(error));
   }
-}
+};
