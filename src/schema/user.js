476d6f36ac1b0d2fc21c77ea28737bd212e464e1
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2025-12-06 22:16:31 +0530

    update schema

--- src/schema/user.js
+++ src/schema/user.js
@@ -6,38 +6,42 @@ const userSchema = new mongoose.Schema(
     email: {
       type: String,
       required: [true, 'Email is required'],
-      unique: [true, 'Email already exist'],
+      unique: [true, 'Email already exists'],
       match: [
-        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-        'Please fill the valid email'
-      ]
-    },
-    userName: {
-      type: String,
-      required: [true, 'User Name is required'],
-      unique: [true, 'User Name already exist'],
-      match: [
-        /^[a-zA-Z0-9]+/,
-        'User name must contain only letters and numbers'
+        // eslint-disable-next-line no-useless-escape
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        'Please fill a valid email address'
       ]
     },
     password: {
       type: String,
       required: [true, 'Password is required']
     },
+    username: {
+      type: String,
+      required: [true, 'Username is required'],
+      unique: [true, 'Username already exists'],
+      minLength: [3, 'Username must be at least 3 characters'],
+      match: [
+        /^[a-zA-Z0-9]+$/,
+        'Username must contain only letters and numbers'
+      ]
+    },
     avatar: {
       type: String
-    }
+    },
   },
   { timestamps: true }
 );
 
 userSchema.pre('save', function saveUser(next) {
-  const user = this;
-  const SALT = bcrypt.genSaltSync(9);
-  const hashPassword = bcrypt.hashSync(user.password, SALT);
-  user.password = hashPassword;
-  user.avatar = `https://robohash.org/${user.userName}`;
+  if (this.isNew) {
+    const user = this;
+    const SALT = bcrypt.genSaltSync(9);
+    const hashedPassword = bcrypt.hashSync(user.password, SALT);
+    user.password = hashedPassword;
+    user.avatar = `https://robohash.org/${user.username}`;
+  }
   next();
 });
 
