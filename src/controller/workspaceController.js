580d919b4a1b72b03f5a9dc1a350d8a734bb2681
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2025-12-10 13:15:57 +0530

    update workspace controller layer

--- src/controller/workspaceController.js
+++ src/controller/workspaceController.js
@@ -1,20 +1,30 @@
-import { StatusCodes } from "http-status-codes"
-import { createWorkspaceService } from "../service/workspaceService"
-import { customErrorResponse, internalErrorResponse, successResponse } from "../utils/common/responseObject"
+import { StatusCodes } from 'http-status-codes';
 
-export const createWorkspace = async(req, res) => {
-    try {
-        const response = await createWorkspaceService({
-            ...req.body,
-            owner: req.user
-        })
-        return res.status(StatusCodes.OK).json(successResponse(response, "Workspace successfully created"))
-    } catch (error) {
-        console.log("Workspace controller error: ",error)
-        if(error.StatusCodes){
-            return res.status(error.StatusCodes).json(customErrorResponse(error))
-        }
+import { createWorkspaceService } from '../service/workspaceService.js';
+import {
+  customErrorResponse,
+  internalErrorResponse,
+  successResponse
+} from '../utils/common/responseObject.js';
 
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(internalErrorResponse(error))
+export const createWorkspace = async (req, res) => {
+  try {
+    const response = await createWorkspaceService({
+      ...req.body,
+      owner: req.id
+    });
+
+    return res
+      .status(StatusCodes.OK)
+      .json(successResponse(response, 'Workspace successfully created'));
+  } catch (error) {
+    console.log('Workspace controller error: ', error);
+    if (error.StatusCodes) {
+      return res.status(error.StatusCodes).json(customErrorResponse(error));
     }
-}
+
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json(internalErrorResponse(error));
+  }
+};
