a5fd678740ded3bede3843351e5802b003bddea3
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2025-12-19 18:33:44 +0530

    Update file

--- src/controller/channelController.js
+++ src/controller/channelController.js
@@ -1,7 +1,10 @@
 import { StatusCodes } from 'http-status-codes';
 import { getChannelByIdService } from '../service/channelService.js';
 import { successResponse } from '../utils/common/responseObject.js';
-import { internalErrorResponse, customErrorResponse } from '../utils/common/responseObject.js';
+import {
+  internalErrorResponse,
+  customErrorResponse
+} from '../utils/common/responseObject.js';
 
 export const getChannelController = async (req, res) => {
   try {
