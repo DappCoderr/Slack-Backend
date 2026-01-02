406afb5751d23bf830bf36e869358bad89106e4b
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2026-01-02 22:36:38 +0530

    add: cors

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import express from 'express';
 import { StatusCodes } from 'http-status-codes';
 
@@ -7,6 +8,7 @@ import apiRouter from './router/apiRouter.js';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', apiRouter);
