60b483abe8dc1f4d33ee38ef4a9d10d7fc2d12ee
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2026-01-12 20:06:58 +0530

    add: new path to workspace

--- src/router/V1/workspace.js
+++ src/router/V1/workspace.js
@@ -3,46 +3,32 @@ import express from 'express';
 import {
   addChannelToWorkspaceController,
   addMemberToWorkspaceController,
-  createWorkspace,
+  createWorkspaceController,
   deleteWorkspaceController,
   getWorkspaceDetailsByIdController,
   getWorkspacesUserIsMemberController,
-  updateWorkspaceController
+  updateWorkspaceController,
+  joinWorkspaceController,
+  resetJoinCodeController,
+  getWorkspaceByJoinCodeController
 } from '../../controller/workspaceController.js';
 import { isAuthenticated } from '../../middleware/isAuthenticated.js';
-import {
-  createWorkspaceSchema,
-  // updateWorkspaceSchema
-} from '../../validation/workspaceSchema.js';
+import { createWorkspaceSchema} from '../../validation/workspaceSchema.js';
 import { validate } from '../../validation/zodValidator.js';
 
 const router = express.Router();
 
 router.get('/', isAuthenticated, getWorkspacesUserIsMemberController);
 router.get('/:workspaceId', isAuthenticated, getWorkspaceDetailsByIdController);
+router.get('/join/:joinCode',isAuthenticated,getWorkspaceByJoinCodeController);
 
-router.post(
-  '/',
-  isAuthenticated,
-  validate(createWorkspaceSchema),
-  createWorkspace
-);
-router.post(
-  '/:workspaceId/members',
-  isAuthenticated,
-  addMemberToWorkspaceController
-);
-router.post(
-  '/:workspaceId/channels',
-  isAuthenticated,
-  addChannelToWorkspaceController
-);
-router.put(
-  '/:workspaceId',
-  isAuthenticated,
-  //validate(updateWorkspaceSchema),
-  updateWorkspaceController
-);
+router.post('/', isAuthenticated, validate(createWorkspaceSchema), createWorkspaceController);
+router.post('/:workspaceId/members', isAuthenticated, addMemberToWorkspaceController);
+router.post('/:workspaceId/channels',isAuthenticated, addChannelToWorkspaceController);
+
+router.put('/:workspaceId',isAuthenticated,updateWorkspaceController);
+router.put('/:workspaceId/join', isAuthenticated, joinWorkspaceController);
+router.put('/:workspaceId/joinCode/reset',isAuthenticated,resetJoinCodeController);
 router.delete('/:workspaceId', isAuthenticated, deleteWorkspaceController);
 
 export default router;
